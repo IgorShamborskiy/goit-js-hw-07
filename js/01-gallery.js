5070582cd804f21e8d9b3be08589da56e3f73e6e
IgorShamborskiy <108817453+IgorShamborskiy@users.noreply.github.com>
2022-12-12 19:41:56 +0200

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryDiv = document.querySelector('.gallery');
+const galleryMakrUp = createGallary (galleryItems)
+
+galleryDiv.insertAdjacentHTML('beforeend', galleryMakrUp)
+
+galleryDiv.addEventListener('click', onCardClick)
+
+function createGallary (galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `
+    })
+        .join(' ');
+}
+function onCardClick(event) {
+    event.preventDefault();
+    if (event.target.tagName !== "IMG") {
+        return
+    } else {
+      const instance = basicLightbox.create(`
+      <div class="modal">
+        <img src="${event.target.dataset.source}" width="800" height="600">
+    </div>`
+      )
+      instance.show();      
+    }
+}
